9f528a2735fc3a63ee79b65b1949021f67253596
WhateverCC <cryptochancc@gmail.com>
2022-12-19 11:30:40 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
 sorry we can't post our code otherwise it will be detected by antivirus soon.
-You're free to build your stealer by using our discord bot.
+You're free to build your stealer by using our discord bot
 
-https://discord.gg/qcbEXYxwCv
+https://discord.gg/aJdsmV79f3
