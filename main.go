b7a50abdd517511ba13d5bf868d169627213b551
WhateverCC <cryptochancc@gmail.com>
2022-12-19 23:37:56 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-sorry we can't post our code otherwise it will be detected by antivirus soon.
+Source code is not public, there is a ton of learning behind this project, this tool will be pay to use as soon as it gets 200 active users!
 You're free to build your stealer by using our discord bot
 
 https://discord.gg/aJdsmV79f3
