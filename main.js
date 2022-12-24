89d0fba9e15a317cd51093137026d05a2b095248
WhateverCC <cryptochancc@gmail.com>
2022-12-24 14:58:40 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 Source code is not public, there is a ton of learning behind this project, this tool will be pay to use as soon as it gets 200 active users!
 You're free to build your stealer by using our discord bot
 
-https://discord.gg/aJdsmV79f3
+discord: https://discord.com/invite/mRprDGJSuP
 website: https://miraistealer.xyz/
